8558aa4d57829fdaddbe257d41b408b542c23f0b
analizajb <96861913+analizajb@users.noreply.github.com>
2022-06-22 19:38:09 -0700

    restarted code

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const routes = require('./controllers/');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Stylesheet
+//express.static() method is a built-in Express.js middleware function that can take all of the contents of a folder and serve them as static assets
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Setting up Handlebars 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
